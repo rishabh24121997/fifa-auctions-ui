2f28223c7ccc6de9be961361aabae41e39d13a1c
ajaynegi1907 <ajaynegi1907@gmail.com>
2021-07-01 21:03:58 +0530

    Created component for displaying squards

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Squads from "./Components/squads"
 
 import Home from './Components/home';
 
@@ -10,6 +11,7 @@ function App() {
       <Router>
         <switch>
           <Route component={Home} exact path="/home"/>
+          <Route component={Squads} exact path="/squads"/>
         </switch>
       </Router>
 
---
+++ src/Components/SquadProps.js
@@ -0,0 +1,105 @@
+export const SquadGridColumns = {
+    PlayerName: "Player Name",
+    Ovr: "Overall",
+    Club: "Club",
+    Position: "Position",
+    Age: "Age",
+    PrefFoot: "Preferred Foot",
+    WeakFoot: "Weak Foot",
+    SkillRating: "Skill Moves",
+    Price: "Price"
+}
+
+export const Squad = [{
+    PlayerName: "Messi",
+    Ovr: "100",
+    Club: "Barcelona",
+    Position: "Any",
+    Age: "34",
+    PrefFoot: "Left",
+    WeakFoot: 5,
+    SkillRating: 5,
+    Price: 10000
+},
+{
+    PlayerName: "Messi",
+    Ovr: "100",
+    Club: "Barcelona",
+    Position: "Any",
+    Age: "34",
+    PrefFoot: "Left",
+    WeakFoot: 5,
+    SkillRating: 5,
+    Price: 10000
+},{
+    PlayerName: "Messi",
+    Ovr: "100",
+    Club: "Barcelona",
+    Position: "Any",
+    Age: "34",
+    PrefFoot: "Left",
+    WeakFoot: 5,
+    SkillRating: 5,
+    Price: 10000
+},{
+    PlayerName: "Messi",
+    Ovr: "100",
+    Club: "Barcelona",
+    Position: "Any",
+    Age: "34",
+    PrefFoot: "Left",
+    WeakFoot: 5,
+    SkillRating: 5,
+    Price: 10000
+},{
+    PlayerName: "Messi",
+    Ovr: "100",
+    Club: "Barcelona",
+    Position: "Any",
+    Age: "34",
+    PrefFoot: "Left",
+    WeakFoot: 5,
+    SkillRating: 5,
+    Price: 10000
+},{
+    PlayerName: "Messi",
+    Ovr: "100",
+    Club: "Barcelona",
+    Position: "Any",
+    Age: "34",
+    PrefFoot: "Left",
+    WeakFoot: 5,
+    SkillRating: 5,
+    Price: 10000
+},{
+    PlayerName: "Messi",
+    Ovr: "100",
+    Club: "Barcelona",
+    Position: "Any",
+    Age: "34",
+    PrefFoot: "Left",
+    WeakFoot: 5,
+    SkillRating: 5,
+    Price: 10000
+},{
+    PlayerName: "Messi",
+    Ovr: "100",
+    Club: "Barcelona",
+    Position: "Any",
+    Age: "34",
+    PrefFoot: "Left",
+    WeakFoot: 5,
+    SkillRating: 5,
+    Price: 10000
+},{
+    PlayerName: "Messi",
+    Ovr: "100",
+    Club: "Barcelona",
+    Position: "Any",
+    Age: "34",
+    PrefFoot: "Left",
+    WeakFoot: 5,
+    SkillRating: 5,
+    Price: 10000
+}
+]
